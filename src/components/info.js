db8c412c77b1c6c8fa7cf3429a45347c8dc8031e
Andre Shkurko <codingdevhub@gmail.com>
2020-03-01 21:11:45 +0200

    added first commit -create-react-app

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Info extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>Weather App on ReactJS</h2>
+                <p>Find out the weather in your city</p>
+            </div>
+        )
+    }
+}
+
+export default Info;
