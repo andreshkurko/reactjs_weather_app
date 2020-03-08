fdba58aad756b97396dd5483f5c6fc1c417ae4e6
Andre Shkurko <codngdevhub@gmail.com>
2020-03-08 22:04:02 +0200

    added weather form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
+
+const API_KEY = "4986bb0847cb5d3ab71bcd83c6dad874";
 
 class App extends React.Component {
+    
+    
+    // Create a method that belongs to this component, the name gettingWeather is any
+    
+    gettingWeather = async (event) => {
+        event.preventDefault();
+
+        const city = event.target.elements.city.value;
+        const api_url = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+        
+    // Convert information to format json
+
+        const data = await api_url.json();
+        console.log(data);
+    }
+
     render() {
         return (
             <div>
                 <Info />
+                <Form weatherMethod={this.gettingWeather}/>
+                <Weather />
             </div>
         );
     }
---
+++ src/components/weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Weather extends React.Component {
+    render() {
+        return(
+            <div>
+              Component with weather information        
+            </div>
+        )
+    }
+}
+
+export default Weather;
