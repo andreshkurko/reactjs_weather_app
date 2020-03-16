6976875ef0c7edbe9f2057dabac2cf6b404f095f
Andre Shkurko <codngdevhub@gmail.com>
2020-03-16 22:13:44 +0200

    simplified the code

--- src/components/info.js
+++ src/components/info.js
@@ -1,14 +1,11 @@
 import React from "react";
 
-class Info extends React.Component {
-    render() {
-        return(
-            <div>
-                <h2>Weather App on ReactJS</h2>
-                <p>Find out the weather in your city</p>
-            </div>
-        )
-    }
-}
+const Info = () => (
+    <div>
+        <h2>Weather App on ReactJS</h2>
+        <p>Find out the weather in your city</p>
+    </div>
+);
+  
 
 export default Info;
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,22 +1,21 @@
 import React from "react";
 
 
-const Weather = (props) => {
-    return (
-        <div>
-            { props.city &&
-                <div>
-                    <p>Location: {props.city}, {props.country}</p>
-                    <p>Temperature: {props.temp}</p>
-                    <p>Location: {props.country}</p>
-                    <p>Pressure: {props.pressure}</p>
-                    <p>Sunrise: {props.sunrise}</p>
-                    <p>Sunset: {props.sunset}</p>
-                </div>
-            }
-            <p>{ props.error }</p>
+const Weather = props => (
+    <div>
+        { props.city &&
+            <div>
+                <p>Location: {props.city}, {props.country}</p>
+                <p>Temperature: {props.temp}</p>
+                <p>Location: {props.country}</p>
+                <p>Pressure: {props.pressure}</p>
+                <p>Sunrise: {props.sunrise}</p>
+                <p>Sunset: {props.sunset}</p>
             </div>
-    );
-}
+        }
+                <p>{ props.error }</p>
+    </div>
+);
+
 
 export default Weather;
